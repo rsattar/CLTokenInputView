556ffcc983cec604c3dde37aea6e7ef79d46702d
Rizwan Sattar <rsattar@gmail.com>
2014-02-24 16:10:33 -0800

    Initial Commit

---
+++ CLTokenInputView/CLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLAppDelegate.h
+//  CLTokenInputView
+//
+//  Created by Rizwan Sattar on 2/24/14.
+//  Copyright (c) 2014 Cluster Labs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CLTokenInputView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CLTokenInputView
+//
+//  Created by Rizwan Sattar on 2/24/14.
+//  Copyright (c) 2014 Cluster Labs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLAppDelegate class]));
+    }
+}
