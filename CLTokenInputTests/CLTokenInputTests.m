7d96746cbf18fc2275c9dac3f918b255bc2bd6ca
Engin Kurutepe <engin@kurutepe.com>
2015-10-21 21:38:45 +0200

    move CLTokenInput to a framework

---
+++ CLTokenInputTests/CLTokenInputTests.m
@@ -0,0 +1,39 @@
+//
+//  CLTokenInputTests.m
+//  CLTokenInputTests
+//
+//  Created by Engin Kurutepe on 21/10/15.
+//  Copyright © 2015 Cluster Labs, Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CLTokenInputTests : XCTestCase
+
+@end
+
+@implementation CLTokenInputTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
