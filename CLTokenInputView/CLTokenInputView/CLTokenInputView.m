46c1f9903bf41c42d2723df1cc7b9e811ef0434d
NakcheonJung <nakcheon.jung@9folders.com>
2017-09-05 17:25:43 +0900

    [*][BUILD] remove ddlog

--- CLTokenInputView/CLTokenInputView/CLTokenInputView.m
+++ CLTokenInputView/CLTokenInputView/CLTokenInputView.m
@@ -352,7 +352,7 @@ static CGFloat const FIELD_MARGIN_X = 4.0; // Note: Same as CLTokenView.PADDING_
     // the deleteBackward on CLTokenView is not called immediately,
     // causing a double-delete
 //    dispatch_async(dispatch_get_main_queue(), ^{
-    DDLogVerbose(@"%s", __PRETTY_FUNCTION__);
+    //DDLogVerbose(@"%s", __PRETTY_FUNCTION__);
     if (textField.text.length == 0) {
         CLTokenView *tokenView = self.tokenViews.lastObject;
         if (tokenView != nil && tokenView.selected == NO) {
