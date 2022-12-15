0b1484d25c4a6c607569ab247b21288c9921b221
Tim Mikelj <timmikelj@monzo.com>
2022-12-15 14:12:55 +0000

    Add SPM support

--- Package.swift
+++ Package.swift
@@ -5,24 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "CLTokenInputView",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CLTokenInputView",
             targets: ["CLTokenInputView"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CLTokenInputView",
-            dependencies: []),
-        .testTarget(
-            name: "CLTokenInputViewTests",
-            dependencies: ["CLTokenInputView"]),
+            resources: [
+                .process("CLTokenInputView.bundle")
+            ]
+        )
     ]
 )
